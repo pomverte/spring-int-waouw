800755e38208aa16ba75d0b699c688ec4191417e
hvle <ha.hong.viet.le@cgi.com>
2015-10-18 19:17:46 +0200

    Spring Boot intialized with https://start.spring.io/ java 1.7 spring integration spring mvc sring social twitter spring boot starter actuator

---
+++ src/main/java/com/fr/cgi/atp/SpringIntWaouwApplication.java
@@ -0,0 +1,12 @@
+package com.fr.cgi.atp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntWaouwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntWaouwApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fr/cgi/atp/SpringIntWaouwApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fr.cgi.atp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringIntWaouwApplication.class)
+@WebAppConfiguration
+public class SpringIntWaouwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
